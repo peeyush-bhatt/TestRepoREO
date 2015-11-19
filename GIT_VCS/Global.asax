5afbfd3bc0257ef7981d430cbbf9fbb563cc9808
peeyush-bhatt <peeyush.bhatt@compunnel.com>
2015-11-19 19:35:02 +0530

    This is REO testing VSC

---
+++ GIT_VCS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GIT_VCS.MvcApplication" Language="C#" %>
---
+++ GIT_VCS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GIT_VCS.Startup))]
+namespace GIT_VCS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
