c2cc48a7db944aefda95822fa23e0487f8290285
peeyush-bhatt <peeyush.bhatt@compunnel.com>
2015-11-19 19:37:40 +0530

    This is test

--- GIT_VCS/App_Start/FilterConfig.cs
+++ GIT_VCS/App_Start/FilterConfig.cs
@@ -8,6 +8,6 @@ namespace GIT_VCS
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-        }
+        } 
     }
 }
